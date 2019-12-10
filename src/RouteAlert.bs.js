e2bdc1d9d701e66a30d9b40f7228e679722fb217
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-12-09 23:17:52 -0500

    Add minutes

--- src/RouteAlert.bs.js
+++ src/RouteAlert.bs.js
@@ -3,68 +3,102 @@
 var Block = require("bs-platform/lib/js/block.js");
 var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
+var Belt_Option = require("bs-platform/lib/js/belt_Option.js");
+var Caml_format = require("bs-platform/lib/js/caml_format.js");
+
+var initialState = {
+  startPoint: undefined,
+  destination: undefined,
+  minutes: undefined
+};
 
 function reducer(state, action) {
-  if (action.tag) {
-    return {
-            startPoint: state.startPoint,
-            destination: action[0]
-          };
-  } else {
-    return {
-            startPoint: action[0],
-            destination: state.destination
-          };
+  switch (action.tag | 0) {
+    case /* SetStartPoint */0 :
+        return {
+                startPoint: action[0],
+                destination: state.destination,
+                minutes: state.minutes
+              };
+    case /* SetDestination */1 :
+        return {
+                startPoint: state.startPoint,
+                destination: action[0],
+                minutes: state.minutes
+              };
+    case /* SetMinutes */2 :
+        return {
+                startPoint: state.startPoint,
+                destination: state.destination,
+                minutes: action[0]
+              };
+    
   }
 }
 
-function startString(startPoint) {
-  if (startPoint !== undefined) {
-    return startPoint;
+function displayString(ostr) {
+  if (ostr !== undefined) {
+    return ostr;
   } else {
     return "nothing";
   }
 }
 
-function start_point_of_input(e) {
+function displayInt(i) {
+  return Belt_Option.mapWithDefault(i, "nada", (function (n) {
+                return String(n);
+              }));
+}
+
+function startPoint(e) {
   var s = e.target.value;
   var s$1 = s === "" ? "nada" : s;
   return /* SetStartPoint */Block.__(0, [s$1]);
 }
 
-function destination_of_input(e) {
+function destination(e) {
   var s = e.target.value;
   var s$1 = s === "" ? "nada" : s;
   return /* SetDestination */Block.__(1, [s$1]);
 }
 
+function minutes(e) {
+  return /* SetMinutes */Block.__(2, [Caml_format.caml_int_of_string(e.target.value)]);
+}
+
 function RouteAlert(Props) {
-  var match = React.useReducer(reducer, {
-        startPoint: undefined,
-        destination: undefined
-      });
+  var match = React.useReducer(reducer, initialState);
   var dispatch = match[1];
   var state = match[0];
   return React.createElement(React.Fragment, undefined, React.createElement("input", {
                   name: "start-point",
                   type: "text",
                   onChange: (function (e) {
-                      return Curry._1(dispatch, start_point_of_input(e));
+                      return Curry._1(dispatch, startPoint(e));
                     })
                 }), React.createElement("input", {
                   name: "destination",
                   type: "text",
                   onChange: (function (e) {
-                      return Curry._1(dispatch, destination_of_input(e));
+                      return Curry._1(dispatch, destination(e));
                     })
-                }), React.createElement("p", undefined, "Start: " + startString(state.startPoint)), React.createElement("p", undefined, "Destination: " + startString(state.destination)));
+                }), React.createElement("input", {
+                  name: "destination",
+                  type: "number",
+                  onChange: (function (e) {
+                      return Curry._1(dispatch, minutes(e));
+                    })
+                }), React.createElement("p", undefined, "Start: " + displayString(state.startPoint)), React.createElement("p", undefined, "Destination: " + displayString(state.destination)), React.createElement("p", undefined, "Minutes: " + displayInt(state.minutes)));
 }
 
 var make = RouteAlert;
 
+exports.initialState = initialState;
 exports.reducer = reducer;
-exports.startString = startString;
-exports.start_point_of_input = start_point_of_input;
-exports.destination_of_input = destination_of_input;
+exports.displayString = displayString;
+exports.displayInt = displayInt;
+exports.startPoint = startPoint;
+exports.destination = destination;
+exports.minutes = minutes;
 exports.make = make;
 /* react Not a pure module */
--- src/RouteAlert.re
+++ src/RouteAlert.re
@@ -1,25 +1,40 @@
-type action = SetStartPoint(string) | SetDestination(string);
+type action = 
+  SetStartPoint(string) 
+  | SetDestination(string)
+  | SetMinutes(int);
 
 type state = {
   startPoint: option(string),
-  destination: option(string)
+  destination: option(string),
+  minutes: option(int)
+};
+
+let initialState = { 
+  startPoint: None, 
+  destination: None,
+  minutes: None
 };
 
 let reducer = (state, action) => {
   switch(action) {
   | SetStartPoint(point) => { ...state, startPoint: Some(point) }
   | SetDestination(dest) => { ...state, destination: Some(dest) }
+  | SetMinutes(minutes) => { ...state, minutes: Some(minutes) }
   };
 };
 
-let startString = (startPoint) => {
-  switch(startPoint) {
+let displayString = (ostr) => {
+  switch(ostr) {
   | Some(s) => s
   | None => "nothing"
   };
 };
 
-let start_point_of_input = (e) => {
+let displayInt = (i) => {
+  Belt.Option.mapWithDefault(i, "nada", (n) => string_of_int(n));
+}
+
+let startPoint = (e) => {
   let s = switch(e->ReactEvent.Form.target##value) {
   | "" => "nada"
   | s => s  
@@ -28,7 +43,7 @@ let start_point_of_input = (e) => {
   SetStartPoint(s);
 };
 
-let destination_of_input = (e) => {
+let destination = (e) => {
   let s = switch(e->ReactEvent.Form.target##value) {
   | "" => "nada"
   | s => s  
@@ -37,15 +52,21 @@ let destination_of_input = (e) => {
   SetDestination(s);
 };
 
+let minutes = (e) => {
+  SetMinutes(int_of_string(e->ReactEvent.Form.target##value))
+};
+
 [@react.component]
 let make = () => {
-  let (state, dispatch) = React.useReducer(reducer, { startPoint: None, destination: None });
+  let (state, dispatch) = React.useReducer(reducer, initialState);
 
   <>
-    <input name="start-point" type_="text" onChange={(e) => dispatch(start_point_of_input(e))}/>
-    <input name="destination" type_="text" onChange={(e) => dispatch(destination_of_input(e))}/>
+    <input name="start-point" type_="text" onChange={(e) => dispatch(startPoint(e))}/>
+    <input name="destination" type_="text" onChange={(e) => dispatch(destination(e))}/>
+    <input name="destination" type_="number" onChange={(e) => dispatch(minutes(e))}/>    
 
-    <p> { React.string("Start: " ++ startString(state.startPoint)) } </p>
-    <p> { React.string("Destination: " ++ startString(state.destination)) } </p>    
+    <p> { React.string("Start: " ++ displayString(state.startPoint)) } </p>
+    <p> { React.string("Destination: " ++ displayString(state.destination)) } </p>    
+    <p> { React.string("Minutes: " ++ displayInt(state.minutes)) } </p>        
   </>;
 };
